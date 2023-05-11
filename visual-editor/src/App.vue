f6339bbbbd9f12fd6e7925657d3911f7a49d071b
Puellaquae <shentukeqin@hotmail.com>
2023-05-11 21:12:04 +0800

    init visual-editor

--- visual-editor/src/App.vue
+++ visual-editor/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="padding: 2rem;">
+    <n-config-provider :locale="zhCN">
+      <n-message-provider>
+        <n-card :hoverable="true" header-style="text-align: center;" title="">
+          <n-h1 style="text-align: center;">可视化编辑工具</n-h1>
+        </n-card>
+      </n-message-provider>
+    </n-config-provider>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { NConfigProvider, NMessageProvider, NCard, NH1 } from "naive-ui"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NConfigProvider,
+    NMessageProvider,
+    NCard,
+    NH1
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
